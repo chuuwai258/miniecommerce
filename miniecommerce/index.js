92c82ce5263d276802f6105275a566df4045036b
chuuwai258 <chuuwaihnin@gmail.com>
2026-05-20 20:14:41 -0700

    second commit

--- miniecommerce/index.js
+++ miniecommerce/index.js
@@ -8,20 +8,16 @@ async function getProduct() {
         const product=data.map(item=>{
             return `
         
-             <div>
-            <center><img src= ${item.image} width="30" height="40"></center><hr><br>
+            <div class="product-container" onclick="productItem(id)">
+            
+            <center><img src= ${item.image} width="30" height="40" class="product-img"></center><br>
         
             <h3>${item.title}</h3><br>
-            <p>Price: ${item.price}</p><br>
-            <p>Category: ${item.category}</p><br>
-            <p>Description: ${item.description}</p>
-        
-
-           </div>
-            `
+            <p><strong>Price: </strong>${item.price}</p><br>
+            <p><strong>Category: </strong>${item.category}</p>
             
-            
-    
+            </div>
+            ` 
         })
         productCard.innerHTML=product.join("")
         
@@ -30,6 +26,31 @@ async function getProduct() {
         }
 }
 getProduct()
+
+async function productItem(id){
+  const singleProduct=document.getElementById("single-container");
+  console.log(id);
+try{
+    const response=await fetch("https://fakestoreapi.com/products")
+    const data = await response.json();
+    window.location.href="item.html"; 
+    const res=`<div>
+    <p>Title:${data.title}</p>
+    </div>
+    `;
+    singleProduct.innerHTML=res;
+    console.log(res);
+    return res;
+}
+catch(error){
+  console.log(error);
+}
+}
+
+
+
+
+
 
 
 
---
+++ miniecommerce/register.js
@@ -0,0 +1,62 @@
+function regBtn() {
+    const firstName = document.querySelector(".firstName").value
+    const lastName = document.querySelector(".lastName").value
+    const email = document.querySelector(".regEmail").value
+    const password = document.querySelector(".pass1").value;
+    const confirmPass = document.querySelector(".pass2").value;
+
+    const user = {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPass
+    };
+
+    if (password === confirmPass) {
+
+        localStorage.setItem("user", JSON.stringify(user));
+
+
+    } else {
+        alert("Password not match");
+    }
+     
+}
+function homePage(event) {
+
+    event.preventDefault();
+
+    const email = document.querySelector(".email").value;
+    const password = document.querySelector(".password").value;
+
+    const savedUser = JSON.parse(localStorage.getItem("user"));
+
+    if (
+        savedUser &&
+        savedUser.email === email &&
+        savedUser.password === password
+    ) {
+
+        localStorage.setItem("isLoggedIn", "true");
+
+        alert("Login successful!");
+
+        window.location.href = "logout.html";
+    }else{
+        alert("Invalid email or password. Please try again.");
+    }
+
+}
+
+
+
+function logoutPage() {
+
+    localStorage.removeItem("isLoggedIn");
+
+    alert("Logout successful!");
+
+    window.location.href = "register.html";
+}
+
